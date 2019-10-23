1d84942447a7dd53b9b28ca027d3bd0ba5278765
Sternosaur <Sternosaur@gmail.com>
2019-10-23 06:34:19 -0500

    uploaded my roll a ball unity project with build
    build is for windows pc

---
+++ Roll a Ball/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //LateUpdate runs every frame but it is garunteed to run after all items have been processed in update.
+    //So when we set the position of the camera, we know absolutely that the player has moved for that frame.
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Roll a Ball/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    //declare Rigidbody
+    private Rigidbody rb;
+    private int count;
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    //typing ctrl + ' did not bring up the scripting API Unity documentation on my windows 10 PC using Microsoft Visual Studio Community 2017
+    {
+      
+    }
+
+    //called every physics step, fixed update intervals are consistent, used for regular updates such as adjusting physics (Rigidbody) objects
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
